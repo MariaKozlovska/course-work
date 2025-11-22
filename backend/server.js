a14279f84bfcde3030092858f0ae12726bc8347e
MariaKozlovska <wow.kozlovska.maria@gmail.com>
2025-11-22 11:28:09 +0200

    added Report Controller and Report Routes

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ const { connect } = require("http2");
 const authRoutes = require("./routes/authRoutes"); 
 const userRoutes = require("./routes/userRoutes");
 const taskRoutes = require("./routes/taskRoutes");
+const reportRoutes = require("./routes/reportRoutes");
 
 const app = express();
 
@@ -32,7 +33,7 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/tasks", taskRoutes);
-// app.use("/api/tasks", reportRoutes);
+app.use("/api/tasks", reportRoutes);
 
 // Start Server
 const PORT = process.env.PORT || 5000; 
